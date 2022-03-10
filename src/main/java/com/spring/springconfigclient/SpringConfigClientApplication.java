4204027e096c7f141b591aeeb71ce86c2cf394d7
nikitapatel029 <nikitapatel.029@gmail.com>
2022-03-10 17:12:46 +0530

    updating latest changes

--- src/main/java/com/spring/springconfigclient/SpringConfigClientApplication.java
+++ src/main/java/com/spring/springconfigclient/SpringConfigClientApplication.java
@@ -7,13 +7,22 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RefreshScope
 @RestController
 @SpringBootApplication
 public class SpringConfigClientApplication {
 
-	@Value("${appName:Config Server is not working. Please check...}")
+	@Value("${appEnvName:Config Server is not working. Please check...}")
 	private String name;
+	
+	
+	@Value("${name: Server is not working. Please check...}")
+	private String defaultName;
+	
+	@Value("${springConfigClientName: Server is not working. Please check...}")
+	private String springConfigClientName;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConfigClientApplication.class, args);
@@ -21,6 +30,7 @@ public class SpringConfigClientApplication {
 
 	@GetMapping("/check/property")
 	public String getName() {
-		return "Hello Viewers, You are using Environemnt - " + this.name;
+		return "Hello Viewers, <br/><br/> You are using Application <b>-" + this.name +"</b><br/><br/>default config-client application properties which is stored at config-client root level is - <b>"+ this.springConfigClientName+ " </b><br/><br/> And default application name which is stored at root level is - <b>"+ this.defaultName+"</b>";
 	}
 }
+
